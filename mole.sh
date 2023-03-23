e34192e140119b35d231bbf4865761d090ae8dfb
Kirill <77616771+danyleyko@users.noreply.github.com>
2023-03-23 01:25:21 +0100

    Add files via upload

--- mole.sh
+++ mole.sh
@@ -29,17 +29,22 @@ fi
 # Define DATE variable
 DATETIME_N=$(date +"%Y-%m-%d_%H-%M-%S")
 
-# Check if FILE or DIRECTORY exist
+# Check if FILE exist
 for arg in "$@"; do
-  if [ -f "$arg" ]; then
-    FILE="$arg"
-    FILE_EXIST=true
-    break
-  elif [ -d "$arg" ]; then
-    DIR="$arg"
-    DIR_EXIST=true
-    break
-  fi
+	if [ -f "$arg" ]; then
+		FILE="$arg"
+		FILE_EXIST=true
+		break
+	fi
+done
+
+# Check if DIRECTORY exist
+for arg in "$@"; do
+	if [ -d "$arg" ]; then
+		DIR="$arg"
+		DIR_EXIST=true
+		break
+	fi
 done
 	
 # Define FILEPATH variable
@@ -74,7 +79,7 @@ open_file(){
 		#vi $FILE
 		
 		if [[ $FILE_EXIST == true &&  $list_arg == false &&  $m_arg == false ]]; then
-  			printf "%s: %s : %s : %s\n" "$(basename "$FILE")" "-" "$DATETIME_N" "$(pwd)"  >> "$LOG"
+  			printf "%s: %s : %s : %s : %s\n" "$(basename "$FILE")" "-" "$DATETIME_N" "$(pwd)" "$FILEPATH" >> "$LOG"
   		else
   			filters $2
 		fi
@@ -83,7 +88,7 @@ open_file(){
 		GROUP=$OPTARG
 		#vi $FILE
 		if [[ $list_arg == false &&  $m_arg == false ]]; then 
-			printf "%s: %s : %s : %s\n" "$(basename "$FILE")" "$GROUP" "$DATETIME_N" "$(pwd)">> "$LOG"
+			printf "%s: %s : %s : %s : %s\n" "$(basename "$FILE")" "$GROUP" "$DATETIME_N" "$(pwd)" "$FILEPATH">> "$LOG"
 		fi
 		
 		;;
@@ -93,107 +98,107 @@ open_file(){
 # Define filters functions
 filters(){
 	# separating string by char=','
-	IFS=',' read -ra array <<< "$1"
-	
-	if [[ "$DIR_EXIST" == false && $secret_log_arg == false ]]; then
-		DIR_1=$(pwd)
-	elif [ $secret_log_arg == true ]; then
-		DIR_1=""
-	else 
-		DIR_1=$DIR
+	if [[ $g_arg == true ]]; then
+		IFS=',' read -ra array <<< "$1"
 	fi
 	
+	if [[ "$DIR_EXIST" == false ]]; then
+		DIR_1=$(pwd)
+		
+	elif [[ $secret_log_arg == true ]]; then
+		DIR_1="$1"
+		
+	elif [[ $DIR_EXIST == true && $g_arg == true ]]; then
+		DIR_1="$2"	
+		
+	elif [[ $DIR_EXIST == true && $g_arg == false ]]; then
+		DIR_1="$1"
+		
+	else 
+		DIR_1=$DIR
+		
+	fi
 	
 	FILTER=$(awk -v a_date="$a_date" -v b_date="$b_date" -v dir="$DIR_1" -v arr="$(echo "${array[@]}")" '
 	    BEGIN {
 	    	n = split(arr, filters, " ");
-	    	
-		if(dir == null)
-		{
-			dir_null=1
-		}
-		else
-		{
-			dir_null=0
-		}
-		
 	    }
 	    {
-				
 	    	
 	    	if (b_date != null && a_date != null) {
 
-			if ((substr($4, 1, 10) >= a_date && substr($4, 1, 10) <= b_date) && (dir == $6 || dir_null == 1)) {
+			if ((substr($4, 1, 10) >= a_date && substr($4, 1, 10) <= b_date) && dir == $6) {
 				if( n > 0 )
 				{
 					for (i=1; i <= n; i++) {
 						if (filters[i] == $2) {
-							printf "%s %s %s\n", $1, $2, $6
+							printf "%s %s %s %s %s\n", $1, $2, $4, $6, $8
 						
 						}
 					}
 				}
-				else if((dir == $6 || dir_null == 1))
+				else if(dir == $6)
 				{
-					printf "%s %s %s\n", $1, $2, $6
+					printf "%s %s %s %s %s\n", $1, $2, $4, $6, $8
 				}
 			}
 		}
 		else if (b_date == null) 
 		{
-
-			if (a_date <= substr($4, 1, 10) && (dir == $6 || dir_null == 1)) 
+	
+			if (a_date <= substr($4, 1, 10) && dir == $6) 
 			{
 				if( n > 0 )
 				{
 					for (i=1; i <= n; i++) {
 						if (filters[i] == $2) {
-							printf "%s %s %s\n", $1, $2, $6
+							printf "%s %s %s %s %s\n", $1, $2, $4, $6, $8
 						
 						}
+						
 					}
 				}
 				else
-				{
-					printf "%s %s %s\n", $1, $2, $6
+				{	
+					printf "%s %s %s %s %s\n", $1, $2, $4, $6, $8
 				}
 				
 			}
-			else if(a_date == null && (dir == $6 || dir_null == 1))
+			else if(a_date == null && dir == $6)
 			{
-				printf "%s %s %s\n", $1, $2, $6
+				printf "%s %s %s %s %s\n", $1, $2, $4, $6, $8
 			}
 		}	
 		else if (a_date == null) 
 		{
 
-			if (substr($4, 1, 10) <= b_date && (dir == $6 || dir_null == 1)) 
+			if (substr($4, 1, 10) <= b_date && dir == $6) 
 			{ 
 				if( n > 0 )
 				{
 					for (i=1; i <= n; i++) {
 						if (filters[i] == $2) {
-							printf "%s %s %s\n", $1, $2, $6
+							printf "%s %s %s %s %s\n", $1, $2, $4, $6, $8
 						
 						}
 					}
 				}
 				else
 				{
-					printf "%s %s %s\n", $1, $2, $6
+					printf "%s %s %s %s %s\n", $1, $2, $4, $6, $8
 				}
 			} 
-			else if(b_date == null && (dir == $6 || dir_null == 1))
+			else if(b_date == null && dir == $6)
 			{
-				printf "%s %s %s\n", $1, $2, $6
+				printf "%s %s %s %s %s\n", $1, $2, $4, $6, $8
 			}
 		}
+		
 	}' "$LOG")
 	
 	if [[ $FILTER == "" ]]; then
 		#FILTER="Empty_list"
-		echo "List is empty" >&2
-		exit 3
+		FILTER="List is empty"
 	fi
 }
 
@@ -215,6 +220,7 @@ case $1 in
 		shift
 		;;
 	-m)	
+		# m
 		m_arg=true
 		shift
 		;;
@@ -241,7 +247,7 @@ case $1 in
 				echo $FILTER
 			    	#vi $FILE
 			else
-			    	printf "%s: %s : %s : %s\n" "$(basename "$FILE")" "-" "$DATETIME_N" "$(pwd)"  >> "$LOG"
+			    	printf "%s: %s : %s : %s : %s\n" "$(basename "$FILE")" "-" "$DATETIME_N" "$(pwd)" "$FILEPATH" >> "$LOG"
 			    	#vi $FILE
 			fi
 		done
@@ -259,8 +265,6 @@ while getopts ":g:m:b:a:h" opt; do
 			echo "Error: '$OPTARG' is not a date" >&2
 			exit 27 
 		fi
-		
-		#shift $((OPTIND - 1))
 		;;
 	 b)
 		if [[ "$OPTARG" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
@@ -269,8 +273,6 @@ while getopts ":g:m:b:a:h" opt; do
 			echo "Error: '$OPTARG' is not a date" >&2
 			exit 27 
 		fi
-		
-		#shift $((OPTIND -1))
 		;;
 	h)
 		print_help
@@ -306,23 +308,64 @@ done
 
 #
 if [[ $list_arg == true ]]; then
-
-	if [[ $g_arg == true ]]; then
+	
+	if [[ $g_arg == true && $DIR_EXIST == true ]]; then
+		filters $2 $DIR
+		
+	elif [[ $g_arg == false && $DIR_EXIST == true ]]; then 
+		filters $DIR
+		
+	elif [[ $g_arg == true && $DIR_EXIST == false ]]; then 
 		filters $2
-	else
+		
+	else	
 		filters
+		
 	fi
-	echo "$FILTER" | awk '{ printf "%s %s\n", $1, $2 }'| column -t 
+	
+	shift $((OPTIND - 1 )) 
+	# Error
+	if [[ ! -d $1  ]]; then
+		echo "Error: You write '$1' which not exist" >&2
+		exit
+	fi
+		
+	if [[ $FILTER == "List is empty" ]]; then
+		echo "$FILTER"
+	else
+		echo "$FILTER" | awk '{ printf "%s %s\n", $1, $2 }'| column -t 
+	fi
+	
 	
 elif [[ $secret_log_arg == true ]]; then
-
+	# Create folder for secret-log
 	if [ ! -d "$HOME/.mole" ]; then
 		mkdir "$HOME/.mole"
 	fi
 	
+	# Parsing DIR
+	if [ $DIR_EXIST == true ] ;then 
+		filters $DIR
+	else
+		filters
+	fi
 	
-	filters
-	echo "$FILTER" | awk '{ printf "%s;%s\n", $3, $2 }'
+	
+	
+	shift $((OPTIND - 1 )) 
+	# Error
+	if [[ ! -d $1  ]]; then
+		echo "Error: You write '$1' which not exist" >&2
+		exit
+	fi
+	
+	
+	if [[ $FILTER == "List is empty" ]]; then
+		echo "$FILTER" 
+	else
+		echo "$FILTER" | awk '{ printf "%s;%s\n", $5, $3 }'
+	fi
+	
 	
 	#> ~/.mole/log_"$USER"_"$DATETIME".bz2
 	
